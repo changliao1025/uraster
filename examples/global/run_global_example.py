d4f85018578e94e3d097ca6295fb0bccdd299c51
changliao1025 <changliao1025@outlook.com>
2025-10-10 11:50:24 -0700

    update example

--- examples/global/run_global_example.py
+++ examples/global/run_global_example.py
@@ -1,7 +1,17 @@
+import os, sys, platform
+sPlatform_os = platform.system()
+
+if sPlatform_os == 'Windows':
+    sPath  = 'C:\\workspace\\python\\uraster\\uraster'
+    sys.path.append(os.path.dirname(sPath))
+    sFilename_target_mesh = 'C:\\scratch\\04model\\pyhexwatershed\\global\\pyflowline20250926004\\mpas.geojson' #use the L10-100 test mesh
+else:
+    sFilename_target_mesh = 'Z:\\04model\\pyhexwatershed\\global\\pyflowline20250926004\\mpas.geojson' #use the L10-100 test mesh
+
 from uraster.classes.uraster import uraster
 
 aConfig=dict()
-sFilename_target_mesh = '/compyfs/liao313/04model/pyhexwatershed/global/pyflowline20250926003/mpas.geojson' #use the L10-100 test mesh
+
 aConfig['sFilename_target_mesh']= sFilename_target_mesh #use the L10-100 test mesh
 aFilename_source_raster = []
 sFilename_hydrosheds_dem = '/compyfs/liao313/00raw/hydrology/hydrosheds/hydrosheds/hyd_glo_dem_15s.tif'
