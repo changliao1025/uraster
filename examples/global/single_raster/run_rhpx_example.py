41e570c958402ca9348ddc8c744fb6b0113eef46
Chang Liao <20618384+changliao1025@users.noreply.github.com>
2025-11-10 16:57:11 -0700

    standalone rebuild topology

--- examples/global/single_raster/run_rhpx_example.py
+++ examples/global/single_raster/run_rhpx_example.py
@@ -15,6 +15,10 @@ else:
         sFilename_source_mesh = '/Users/liao313/workspace/python/uraster/data/input/mesh/gdf_rhpx_res3.geojson' #use the L10-100 test mesh
         sFilename_raster = '/Users/liao313/workspace/python/uraster/data/input/raster/edgar_MNM_2015.tiff'
         sFilename_target_mesh = '/Users/liao313/workspace/python/uraster/data/output/rhpx/rhpx_res3_uraster.geojson'
+        sFilename_mesh_png = '/Users/liao313/workspace/python/uraster/data/output/rhpx/mesh.jpg'
+        sFilename_raster_png = '/Users/liao313/workspace/python/uraster/data/output/rhpx/raster.png'
+        sFilename_variable_png = '/Users/liao313/workspace/python/uraster/data/output/rhpx/uraster.png'
+        sFilename_variable_animation = '/Users/liao313/workspace/python/uraster/data/output/rhpx/global_uraster.mp4'
 
 
 from uraster.classes.uraster import uraster
@@ -36,6 +40,7 @@ def main():
     dLongitude_focus_in = -112.033964
     dLatitude_focus_in = 43.491977
     pRaster.visualize_source_mesh(sFilename_out=sFilename_mesh_png, dLongitude_focus_in=dLongitude_focus_in, dLatitude_focus_in=dLatitude_focus_in)
+    pRaster.visualize_raster(sFilename_out=sFilename_raster_png )
 
     pRaster.run_remap()
     #pRaster.report_outputs() #not implemented yet
