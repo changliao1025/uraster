c3f340e14cc68c419e441f9ad3284cd65a8d70d9
changliao1025 <changliao1025@outlook.com>
2026-02-27 13:29:57 -0800

    version info update

--- uraster/__init__.py
+++ uraster/__init__.py
@@ -2,7 +2,7 @@
 uraster - A Python package for unstructured raster processing and remapping
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.7"
 __author__ = "Chang Liao"
 __email__ = "changliao.climate@gmail.com"
 
